c68877e0a35323227688c8229bfa58fbcb8afce5
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 11:11:55 +0100

    Création de l'exercice 2

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$nom_du_visiteur = 'Michalowski';
+$prenom_du_visiteur = 'Pauline';
+$age_du_visiteur = 23;
+echo 'Le visiteur s\'appelle ' . $prenom_du_visiteur . ' ' . $nom_du_visiteur . ' et a ' . $age_du_visiteur . ' ans.'
+?>
